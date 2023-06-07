9d174edacbe5981f491e7ab713e2e214333154ee
Spicely <Spicely@outlook.com>
2023-06-07 23:40:36 +0800

    v0.0.1

---
+++ lib/openim_manager.dart
@@ -0,0 +1,15 @@
+library openim_manager;
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+import 'package:flutter_muka/flutter_muka.dart';
+import 'package:flutter_openim_sdk/flutter_openim_sdk.dart' hide Utils;
+
+export 'package:flutter_openim_sdk/flutter_openim_sdk.dart' hide Utils;
+
+part 'src/openim_listener.dart';
+part 'src/openim_manager.dart';
